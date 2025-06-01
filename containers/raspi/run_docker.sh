826bb2dd8b4dc1aa34f6575e20966ac93d16dd82
EashanVytla <Eashan.Vytla@gmail.com>
2025-06-01 14:11:45 -0400

    modified container name in the scripts

--- containers/raspi/run_docker.sh
+++ containers/raspi/run_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-IMAGE_NAME="dreamer_docker" # Leaving this as dreamer_docker for now because I don't want to build a redundant docker on the raspi
-CONTAINER_NAME="dreamer_container"
+IMAGE_NAME="bv_container" # Leaving this as dreamer_docker for now because I don't want to build a redundant docker on the raspi
+CONTAINER_NAME="bv_container"
 HOST_DIR="/home/eashan/bv_ws"
 CONTAINER_SRC_DIR="/bv_ws"
 
